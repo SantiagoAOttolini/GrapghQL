992bf54c1c616409148d2882628b43836cab0d2b
santiago <santiago.ottolini97@hotmail.com>
2021-05-19 14:42:56 -0300

    Enum and Fragments

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,12 +1,10 @@
-import {GraphQLSchema} from 'graphql'
-import {makeExecutableSchema} from 'graphql-tools'
-import 'graphql-import-node'
-import rootSchema from "./schemas/schema.graphql"
-import resolvers from './resolvers/resolversMap'
+import { GraphQLSchema } from "graphql";
+import { makeExecutableSchema } from "graphql-tools";
+import "graphql-import-node";
+import rootSchema from "./schemas/schema.graphql";
+import resolvers from "./resolvers/resolversMap";
 
 export const schema: GraphQLSchema = makeExecutableSchema({
-    typeDefs:[
-        rootSchema
-    ],
-    resolvers
-})
+  typeDefs: [rootSchema],
+  resolvers,
+});
--- src/graphql/resolvers/resolversMap.ts
+++ src/graphql/resolvers/resolversMap.ts
@@ -1,23 +1,25 @@
-import {IResolvers} from 'graphql-tools'
+import { IResolvers } from "graphql-tools";
 
 const resolvers: IResolvers = {
-    Query:{
-        hello(){
-            return "world"
+  Query: {
+    hello() {
+      return "world";
+    },
+    getCharacters() {
+      return [
+        {
+          id: 1,
+          name: "Link",
+          race: "Dog",
         },
-        getCharacters(){
-            return[{
-                id: 1,
-                name: "Link",
-                race: "Dog"
-            },
-            {
-                id: 2,
-                name: "Darck",
-                race: "Cat"
-            }]
-        }
-    }
-}
+        {
+          id: 2,
+          name: "Darck",
+          race: "Cat",
+        },
+      ];
+    },
+  },
+};
 
-export default resolvers
+export default resolvers;
