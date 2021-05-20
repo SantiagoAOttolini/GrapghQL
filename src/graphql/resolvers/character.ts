a4c030ed370356761d86506ee6b2770a9410affd
santiago <santiago.ottolini97@hotmail.com>
2021-05-20 14:21:40 -0300

    Mutation: create a new character

--- src/graphql/resolvers/character.ts
+++ src/graphql/resolvers/character.ts
@@ -6,18 +6,24 @@ export const characterResolver: IResolvers = {
     getCharacters() {
       return data.characters;
     },
-    getCharacter(root: void, args: any){
-      const [find] = data.characters.filter(ch => ch._id === args._id)
-      return find
-    }
+    getCharacter(root: void, args: any) {
+      const [find] = data.characters.filter((ch) => ch._id === args._id);
+      return find;
+    },
+  },
+  Mutation: {
+    createCharacter(root: void, args: any) {
+      data.characters.push(args.character);
+      return "Character added successfully";
+    },
   },
   Character: {
-    games(root: any){
+    games(root: any) {
       const gameList: Array<any> = [];
       root.games.map((gameId: string) =>
-      gameList.push(...data.games.filter(game => game._id === gameId))
-      )
-      return gameList
-    }
-  }
+        gameList.push(...data.games.filter((game) => game._id === gameId))
+      );
+      return gameList;
+    },
+  },
 };
